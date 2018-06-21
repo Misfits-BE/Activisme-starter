f98d547894c193eba8af6772ad81612b834be0e0
Tim Joosten <Tjoosten3@gmail.com>
2018-06-21 17:10:30 +0200

    Splitout logout function
    Split out the logout function form the dropdown. And replace the logout link with a power off symbol in the navbar.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,22 +48,20 @@
                                 <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </li>
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">
+                                    {{ Auth::user()->name }}
                                 </a>
+                            </li>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('logout') }}">
+                                    <i class="fas fa-power-off"></i>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
-                                </div>
+                                </a>
                             </li>
                         @endguest
                     </ul>
